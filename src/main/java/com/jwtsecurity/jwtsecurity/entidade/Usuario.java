cf62709bf9e767bc823d5b8f295b1fe5f3de51aa
Maksuedson <maksuedson@hotmail.com>
2023-10-17 17:09:08 -0300

    Adicionado implements UserDetails

--- src/main/java/com/jwtsecurity/jwtsecurity/entidade/Usuario.java
+++ src/main/java/com/jwtsecurity/jwtsecurity/entidade/Usuario.java
@@ -1,5 +1,13 @@
 package com.jwtsecurity.jwtsecurity.entidade;
 
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,7 +16,8 @@ import lombok.Data;
 
 @Data
 @Entity
-public class Usuario {
+public class Usuario implements UserDetails, Serializable{
+	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,4 +25,29 @@ public class Usuario {
     private String login;
     private String password;
     private String role;
+    
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority("ROLE-USER"));
+	}
+	@Override
+	public String getUsername() {
+		return login;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
