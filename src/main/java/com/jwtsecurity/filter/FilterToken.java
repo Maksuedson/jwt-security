af1537eb1d93a9892398f5f6e7434b812ec4fd36
Maksuedson <maksuedson@hotmail.com>
2023-10-20 17:52:57 -0300

    Adicionando response de usuario nao autorizado

--- src/main/java/com/jwtsecurity/filter/FilterToken.java
+++ src/main/java/com/jwtsecurity/filter/FilterToken.java
@@ -13,15 +13,16 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jwtsecurity.repositorios.UsuarioRepositorio;
 import com.jwtsecurity.services.TokenService;
 
 @Component
-public class FilterToken extends OncePerRequestFilter{
-	
+public class FilterToken extends OncePerRequestFilter {
+
 	@Autowired
 	private TokenService tokenService;
-	
+
 	@Autowired
 	private UsuarioRepositorio usuarioRepositorio;
 
@@ -29,21 +30,34 @@ public class FilterToken extends OncePerRequestFilter{
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 		String token;
-		
+
 		var authorizationHeader = request.getHeader("Authorization");
-		
+
 		if (authorizationHeader != null) {
 			token = authorizationHeader.replace("Bearer ", "");
-			
-			var subject  = this.tokenService.getSubject(token);
+
+			var subject = this.tokenService.getSubject(token);
 			var usuario = this.usuarioRepositorio.findByLogin(subject);
 			var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
-			
-			SecurityContextHolder.getContext().setAuthentication(authentication);
+
+			if (authentication != null) {
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+			}
+
 		}
-		
+
+		else {
+			System.out.println("Erro no token");
+			ErroDTO erro = new ErroDTO(401, "Usuario nao autorizado para este sistema");
+			response.setStatus(erro.getStatus());
+			response.setContentType("application/json");
+			ObjectMapper mapper = new ObjectMapper();
+			response.getWriter().print(mapper.writeValueAsString(erro));
+			response.getWriter().flush();
+			return;
+		}
+
 		filterChain.doFilter(request, response);
 	}
 
-	
 }
